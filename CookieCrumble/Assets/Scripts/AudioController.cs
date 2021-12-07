1931b45a14e563019865978276113e94d78c02c3
Nataliia Arsenieva <nataly.arsenieva@gmail.com>
2021-12-07 07:55:35 -0500

    Added background level noise

---
+++ CookieCrumble/Assets/Scripts/AudioController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        audio.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
