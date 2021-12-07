948b3f1bf3eb311f10b441336457701b2f20c1e9
Nataliia Arsenieva <nataly.arsenieva@gmail.com>
2021-12-07 04:54:41 -0500

    Level Design updates

--- CookieCrumble/Assets/Scripts/MenuPlay.cs
+++ CookieCrumble/Assets/Scripts/MenuPlay.cs
@@ -8,6 +8,5 @@ public class MenuPlay : MonoBehaviour
    public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        Debug.Log("wow");
     }
 }
