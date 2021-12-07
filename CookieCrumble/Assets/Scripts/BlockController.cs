5d8dc58d880dce2b7ddc9a2e211a5d67e3a9b6a0
Victoria <victorialiu38@gmail.com>
2021-12-07 02:31:06 -0500

    tiles and level design

--- CookieCrumble/Assets/Scripts/BlockController.cs
+++ CookieCrumble/Assets/Scripts/BlockController.cs
@@ -5,31 +5,55 @@ using UnityEngine;
 public class BlockController : MonoBehaviour
 {
     public bool pink, brown, vanilla;
+
+    public Vector3 initialPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        initialPos = this.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        if(this.transform.position.y <= -15)
+        {
+            this.transform.position = initialPos;
+        }
     }
+
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
-            if ((PlayerController.color == "Vanilla" && vanilla) || (PlayerController.color == "Pink" && pink) || (PlayerController.color == "Brown" && brown) )
-                this.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+            if ((PlayerController.color != "Vanilla" && vanilla) || (PlayerController.color != "Pink" && pink) || (PlayerController.color != "Brown" && brown))
+                this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+            else 
+            {
+                this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+                this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+            }
             
+
+          
+
         }
+
+
+        if (collision.gameObject.CompareTag("vanillaBlock"))
+        {
+            this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+        }
+
+
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            this.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+            //this.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
         }
     }
 }
