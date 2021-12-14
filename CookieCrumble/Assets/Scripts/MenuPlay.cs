a1f523650bcce339d84b6006e70bbf6c146dfcf4
Victoria <victorialiu38@gmail.com>
2021-12-14 08:20:18 -0500

    option menu

--- CookieCrumble/Assets/Scripts/MenuPlay.cs
+++ CookieCrumble/Assets/Scripts/MenuPlay.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuPlay : MonoBehaviour
 {
@@ -17,6 +18,22 @@ public class MenuPlay : MonoBehaviour
 
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         
+    }
+
+    public void LOAD_SCENE(string SceneName)
+    {
+        //Debug.Log(SceneName);
+        SceneManager.LoadScene(SceneName);
+    }
+
+    public void AudioMute()
+    {
+        AudioListener.volume = 0f;
+    }
+
+    public void AudioUnmute()
+    {
+        AudioListener.volume = 1f;
     }
 
 
